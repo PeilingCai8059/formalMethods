d014dab61fea38c35510a28e12af0faa22060062
sayali-sarode <sayali.a.sarode@gmail.com>
2023-11-26 22:35:36 -0600

    Added changes for EF, AF, EG, AG, E[ p U q ], A[ p U q ]]

--- view/src/main/java/modelCheckCTL/controller/CTL_Checker.java
+++ view/src/main/java/modelCheckCTL/controller/CTL_Checker.java
@@ -68,10 +68,118 @@ public class CTL_Checker {
                 }
             }
             return truthValueForEX;
-        }
+        } else if(symbol.equals("A") && node.getRightExpression().getNodeName().equals("F")) {
+            ExpressionNode subFormula = node.getRightExpression().getRightExpression();
 
+            // Check if there exists a state that satisfies the subformula
+            boolean truthValueForAF = false;
+
+            for (State state : ks.getStates()) {
+                boolean satisfiesSubFormula = SAT(subFormula, state.getStateName());
+
+                if (satisfiesSubFormula) {
+                    truthValueForAF = true;
+                    break;
+                }
+            }
+
+            return truthValueForAF;
+        } else if(symbol.equals("E") && node.getRightExpression().getNodeName().equals("F")) {
+            boolean truthValueForEF = false;
+            ExpressionNode subFormula = node.getRightExpression().getRightExpression();
+
+            for (State state : ks.getStates()) {
+                boolean satisfiesSubFormula = SAT(subFormula, state.getStateName());
+
+                if (satisfiesSubFormula) {
+                    truthValueForEF = true;
+                    break;
+                }
+            }
+
+            return truthValueForEF;
+        } else if(symbol.equals("A") && node.getRightExpression().getNodeName().equals("G")) {
+            boolean truthValueForAG = true;
+            ExpressionNode subFormula = node.getRightExpression().getRightExpression();
+            for (Transition transition : ks.getTransitions()) {
+                if (!SAT(subFormula, transition.getToState().getStateName())) {
+                    truthValueForAG = false;
+                    break;
+                }
+            }
+            return truthValueForAG;
+        } else if(symbol.equals("E") && node.getRightExpression().getNodeName().equals("G")) {
+            boolean truthValueForEG = false;
+            ExpressionNode subFormula = node.getRightExpression().getRightExpression();
+
+            for (State state : ks.getStates()) {
+                boolean satisfiesSubFormula = SAT(subFormula, state.getStateName());
+
+                if (!satisfiesSubFormula) {
+                    truthValueForEG = false;
+                    break;
+                }
+
+                truthValueForEG = true;
+            }
+
+            return truthValueForEG;
+        } else if(symbol.equals("A") && node.getRightExpression().getNodeName().equals("U")) {
+            boolean truthValueForAU = true;
+            ExpressionNode subLeftFormula = node.getRightExpression().getLeftExpression();
+            ExpressionNode subRightFormula = node.getRightExpression().getRightExpression();
+
+            for (Transition transition : ks.getTransitions()) {
+                if (transition.getFromState().getStateName().equals(stateName)) {
+                    boolean leftSatisfies = SAT(subLeftFormula, transition.getToState().getStateName());
+                    boolean rightSatisfies = SAT(subRightFormula, transition.getToState().getStateName());
+
+                    if (!rightSatisfies) {
+                        truthValueForAU = false;
+                        break;
+                    }
+                    if (leftSatisfies) {
+                        truthValueForAU = true;
+                        break;
+                    }
+                }
+            }
+
+            return truthValueForAU; 
+        } else if(symbol.equals("E") && node.getRightExpression().getNodeName().equals("U")) {
+            boolean truthValueForEU = false;
+            ExpressionNode subLeftFormula = node.getRightExpression().getLeftExpression();
+            ExpressionNode subRightFormula = node.getRightExpression().getRightExpression();
+
+            // Check if there exists a state that satisfies the subRightFormula
+            for (State state : ks.getStates()) {
+                boolean rightSatisfies = SAT(subRightFormula, state.getStateName());
+
+                if (rightSatisfies) {
+                    truthValueForEU = true;
+                    break;
+                }
+            }
+
+            // Check if there exists a path from the current state satisfying the subLeftFormula until subRightFormula is satisfied
+            boolean leftSatisfiesForSomeTransition = false;
+
+            for (Transition transition : ks.getTransitions()) {
+                if (transition.getFromState().getStateName().equals(stateName)) {
+                    boolean leftSatisfies = SAT(subLeftFormula, transition.getToState().getStateName());
+                    boolean rightSatisfies = SAT(subRightFormula, transition.getToState().getStateName());
+
+                    leftSatisfiesForSomeTransition = leftSatisfiesForSomeTransition || (leftSatisfies && rightSatisfies);
+
+                    if (rightSatisfies) {
+                        truthValueForEU = true;
+                        break;
+                    }
+                }
+            }
+
+            return truthValueForEU || leftSatisfiesForSomeTransition;
+        }
         return false;
     }
-
-
 }
